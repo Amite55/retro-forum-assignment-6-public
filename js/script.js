7963744a59811734bb969ed5fdd26b8199c52673
Amite55 <amite403@gmail.com>
2024-03-03 09:46:53 -0800

    main mark work done

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ const loadData = async () =>{
         const div = document.createElement('div');
        div.innerHTML=`<div class="flex gap-7 my-7 bg-slate-700 py-5 px-3 rounded-lg drop-shadow-md">
        <div>
-         <img src="${element.image}" alt="">
+         <img class="w-10 rounded-full border" src="${element.image}" alt="">
        </div>
        <div class="space-y-5">
          <div class="flex gap-4">
@@ -18,7 +18,6 @@ const loadData = async () =>{
          </div>
          <h4 class="font-bold text-2xl">${element.title}</h4>
          <p>${element.description}</p>
-
          <div class="flex justify-between">
            <div class="flex gap-4">
              <p><i class="fa-regular fa-message"></i> <span>${element.comment_count}</span></p>
@@ -43,23 +42,20 @@ const loadData = async () =>{
             count++;
             const countNum = document.getElementById('count-num');
             countNum.innerText= count;
-            // console.log(event.target.parentNode.parentNode.parentNode.childNodes[3].innerText)
-
+           
+            // counter and viewer counter
             const titleName = event.target.parentNode.parentNode.parentNode.childNodes[3].innerText;
 
-            // console.log(event.target.parentNode.parentNode.parentNode.childNodes[7].childNodes[1].childNodes[3].outerText);
+          
             const countView = event.target.parentNode.parentNode.parentNode.childNodes[7].childNodes[1].childNodes[3].outerText;
 
-            // const titleCard = document.getElementById('title-Card');
-            // const viewCouter = document.getElementById('view-counter')
-            // titleCard.innerText= titleName;
-            // viewCouter.innerText= countView;
+
 
             const createDiv = document.createElement('div');
 
            createDiv.innerHTML=`<div class="flex gap-8 bg-zinc-700 py-7 rounded-lg px-3 my-5">
             <p id="title-Card">${titleName}</p>
-            <p id="view-counter"><i class="fa-regular fa-eye"></i> <span>${countView}</span></p>
+            <p id="view-counter" class ="text-center"><i class="fa-regular fa-eye"></i> <span>${countView}</span></p>
           </div>`;
           cardShowTitleViewr.appendChild(createDiv)
 
@@ -68,9 +64,50 @@ const loadData = async () =>{
 
 }
 
+const postData = async () =>{
+    const res = await fetch('https://openapi.programming-hero.com/api/retro-forum/latest-posts')
+    const allData = await res.json();
+    const letsData = document.getElementById('all-data');
+    allData.forEach(data => {
+        const divu = document.createElement('div');
+
+
+
+
+        // divu.classList=`grid sm:grid-cols-1 lg:grid-cols-3`;
+
+
+
+        divu.innerHTML=`<div class="card bg-base-100 shadow-xl border border-sky-500">
+        <figure class="px-10 pt-10">
+          <img src="${data.cover_image}" alt="Shoes" class="rounded-xl" />
+        </figure>
+        <div class="card-body">
+          <div class="flex ">
+            <i class="fa-regular fa-calendar-days"></i>
+            <p>${data?.author.posted_date}</p>
+          </div>
+          <h2 class="card-title">${data.title}</h2>
+          <p>${data.description}</p>
+          <div class="flex gap-5">
+            <img class="w-10 rounded-full" src="${data.profile_image}" alt="">
+            <div>
+              <h4 class="text-xl font-bold">${data.author.name}</h4>
+              <p>${data.author?.designation}</p>
+            </div>
+          </div>
+        </div>
+      </div>`;
+      letsData.appendChild(divu)
+
+    })
+}
 
 
 
 
 
-loadData()
+
+loadData();
+
+postData();
